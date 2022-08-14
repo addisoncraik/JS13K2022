74925a48ec0d9a3555cab4d0bb28907d39697e72
Craiky <31390334+Craiky@users.noreply.github.com>
2022-08-14 14:45:07 -0700

    Update mazeGeneration.js
    Updated the maze generation code, more efficient

--- src/mazeGeneration.js
+++ src/mazeGeneration.js
@@ -3,7 +3,7 @@ let groups = []
 let edges = []
 
 let mapSize = 30
-let tileSize = 20
+let tileSize = 10
 
 
 class Cell{
@@ -12,7 +12,7 @@ class Cell{
         this.j = j //y
 
         this.group = group
-        this.edges = [1,1,1,1] //t,r,b,l
+        this.edges = [1,1,0,0] //t,r,b,l
     }
 
     draw(){
@@ -28,105 +28,32 @@ class Cell{
     }
 
     remove(edge, side){
+        
+        let neighbour = side ? cells[this.i+1][this.j] : cells[this.i][this.j-1]
 
-        if(side == 0 && this.j-1 >= 0){
-
-            let neighbour = cells[this.i][this.j-1]
-
-            if(neighbour.group != this.group){
-                neighbour.edges[2] = 0
-                edges.splice(edge,1)
-
-                for(let i = 0; i<groups[neighbour.group].length; i++){
-                    groups[this.group].push(groups[neighbour.group][i])
-                }
-
-                groups.splice(neighbour.group,1)
-
-                for(let i = neighbour.group < this.group ? neighbour.group : this.group; i < groups.length; i++){
-                    for(let j = 0; j<groups[i].length; j++){
-                        groups[i][j].group = i
-                        cells[groups[i][j].i][groups[i][j].j].group = i
-                    }
-                }
-
-                this.edges[side] = 0
-                return
-            }
+        if(neighbour.group != this.group){
+            this.edges[side] = 0
+            neighbour.edges[side+2] = 0
+            edges.splice(edge,1)
+            this.delete(neighbour)
+            return
         }
-       else if(side == 1 && this.i+1 < mapSize){
+    }
 
-            let neighbour = cells[this.i+1][this.j]
+    delete(neighbour){
 
-            if(neighbour.group != this.group){
-                neighbour.edges[3] = 0
-                edges.splice(edge,1)
+        
 
-                for(let i = 0; i<groups[neighbour.group].length; i++){
-                    groups[this.group].push(groups[neighbour.group][i])
-                }
-
-                groups.splice(neighbour.group,1)
-
-                for(let i = neighbour.group < this.group ? neighbour.group : this.group; i < groups.length; i++){
-                    for(let j = 0; j<groups[i].length; j++){
-                        groups[i][j].group = i
-                        cells[groups[i][j].i][groups[i][j].j].group = i
-                    }
-                }
-
-                this.edges[side] = 0
-                return
-            }
+        for(let i = 0; i<groups[neighbour.group].length; i++){
+            groups[this.group].push(groups[neighbour.group][i])
         }
-        else if(side == 2 && this.j+1 < mapSize){
 
-            let neighbour = cells[this.i][this.j+1]
+        groups.splice(neighbour.group,1)
 
-            if(neighbour.group != this.group){
-                neighbour.edges[0] = 0
-                edges.splice(edge,1)
-
-                for(let i = 0; i<groups[neighbour.group].length; i++){
-                    groups[this.group].push(groups[neighbour.group][i])
-                }
-
-                groups.splice(neighbour.group,1)
-
-                for(let i = neighbour.group < this.group ? neighbour.group : this.group; i < groups.length; i++){
-                    for(let j = 0; j<groups[i].length; j++){
-                        groups[i][j].group = i
-                        cells[groups[i][j].i][groups[i][j].j].group = i
-                    }
-                }
-
-                this.edges[side] = 0
-                return
-            }
-        }
-        else if(side == 3 && this.i-1 >= 0){
-
-            let neighbour = cells[this.i-1][this.j]
-
-            if(neighbour.group != this.group){
-                neighbour.edges[1] = 0
-                edges.splice(edge,1)
-
-                for(let i = 0; i<groups[neighbour.group].length; i++){
-                    groups[this.group].push(groups[neighbour.group][i])
-                }
-
-                groups.splice(neighbour.group,1)
-
-                for(let i = neighbour.group < this.group ? neighbour.group : this.group; i < groups.length; i++){
-                    for(let j = 0; j<groups[i].length; j++){
-                        groups[i][j].group = i
-                        cells[groups[i][j].i][groups[i][j].j].group = i
-                    }
-                }
-
-                this.edges[side] = 0
-                return
+        for(let i = neighbour.group < this.group ? neighbour.group : this.group; i < groups.length; i++){
+            for(let j = 0; j<groups[i].length; j++){
+                groups[i][j].group = i
+                cells[groups[i][j].i][groups[i][j].j].group = i
             }
         }
     }
@@ -146,29 +73,31 @@ for(let i = 0; i< mapSize; i++){
         if(i+1 < mapSize){
             edges.push({i:i,j:j,s:1})
         }
-        if(j+1 < mapSize){
-            edges.push({i:i,j:j,s:2})
+        if(j-1 >= 0){
+            edges.push({i:i,j:j,s:0})
         }
     }
 }
 
 function removeWall(){
     while(groups.length > 1){
-        ctx.clearRect(0,0,canvas.width,canvas.height)
-
         let rE = randInt(edges.length)
         cells[edges[rE].i][edges[rE].j].remove(rE,edges[rE].s)
 
-        for(let i = 0; i < mapSize; i++){
-            for(let j = 0; j < mapSize; j++){
-                cells[i][j].draw()
-            }
-        }
+        
     }
 }
 
 
 removeWall()
+
+ctx.clearRect(0,0,canvas.width,canvas.height)
+        for(let i = 0; i < mapSize; i++){
+            for(let j = 0; j < mapSize; j++){
+                cells[i][j].draw()
+            }
+        }
+
 
 
 function randInt(max, min = 0){
